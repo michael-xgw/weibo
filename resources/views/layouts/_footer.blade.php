dddae38b3a7b14580b335797d7566b5cec93b1ea
michael-xgw <1020846675@qq.com>
2020-06-29 17:09:10 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+<img class="brand-icon" src="http://i1.sinaimg.cn/dy/images/header/2009/standardl2nav_sina_new.gif">
+<a href="https://laravel-china.org/courses" target=_blank>
+刻意练习，每日精进
+</a>
+<div class="float-right">
+<a href="/about" >关于</a>
+</div>
+</footer>
