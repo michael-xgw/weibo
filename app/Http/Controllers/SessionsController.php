280c84e64f6bdf11305901c1237576a747452e9a
michael-xgw <1020846675@qq.com>
2020-06-30 20:14:01 +0800

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -17,7 +17,7 @@ class SessionsController extends Controller
             'password'=>'required',
         ]);
 
-        if(Auth::attempt($credentials)){
+        if(Auth::attempt($credentials,$request->has('remember'))){
             session()->flash('success','wellcome back');
             return redirect()->route('users.show',[Auth::user()]);
         }else{
